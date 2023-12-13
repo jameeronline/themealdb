0e2cf08fd57bb00e0da713b5279aa3e103479b1b
Jamal Mohamed Ameer <jamalbe2004@gmail.com>
2023-12-13 22:20:18 +0300

    CMS contents added

--- src/components/pages/blog/index.jsx
+++ src/components/pages/blog/index.jsx
@@ -24,7 +24,7 @@ export default function Blog() {
         setPosts(response);
         setIsLoading(false);
 
-        console.log("All of your products:", response);
+        //console.log("All of your products:", response);
         //console.log(Object.keys(response));
       } catch (error) {
         // handle any errors
